2732be0d57cd150adecdd284bb7deb37fabefda7
UJJWAL <122596469+dahalujjwal@users.noreply.github.com>
2024-07-16 20:36:09 +0545

    File Upload Added

--- content_writing/models.py
+++ content_writing/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 class ContentForm(models.Model):
+    department_choices = [
+
+    ('BCT', 'Computer Engineering'),
+    ('BCE', 'Civil Engineering'),
+    ('BEL', 'Electrical Engineering'),
+    ('BEI', 'Electronics, Communication and Information Engineering'),
+    ('BME', 'Mechanical Engineering'),
+    ('BIE', 'Industrial Engineering'),
+    ('BAM', 'Automobile Engineering'),
+]
+    
     name = models.CharField(max_length=100)
     roll_number = models.IntegerField(unique=True)
     email = models.EmailField(unique=True,max_length=200)
     phone = models.IntegerField(unique=True)
+    department = models.CharField(choices=department_choices,max_length=100,null=True)
+    file = models.FileField(upload_to="articles/",null=True)
     
